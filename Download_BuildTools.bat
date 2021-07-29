125c79e232bfe9533eff1725ffa3a4a54d9de7aa
xxxMEMESCOEPxxx <67339620+xxxMEMESCOEPxxx@users.noreply.github.com>
2021-07-29 10:22:07 -0400

    Update Download_BuildTools.bat

--- Download_BuildTools.bat
+++ Download_BuildTools.bat
@@ -1,3 +1,10 @@
 @echo off
-
+Mkdir buildtools
+Cd buildtools
 Echo Downloading BuildTools from Github repo...
+
+
+Powershell -Command "Invoke-WebRequest https://github.com/py2exe/py2exe/releases/download/v0.10.4.0/py2exe-0.10.4.0.tar.gz -Outfile py2exe.tar.gz"
+Echo Expanding archive...
+
+Wsl gzip -d py2exe.tar.gz
