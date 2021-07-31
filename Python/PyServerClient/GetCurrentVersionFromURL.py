942a95b83c70509b859ce67fadb23abb3d755399
xxxMEMESCOEPxxx <67339620+xxxMEMESCOEPxxx@users.noreply.github.com>
2021-07-31 11:38:29 -0400

    Create GetCurrentVersionFromURL.py

--- Python/PyServerClient/GetCurrentVersionFromURL.py
+++ Python/PyServerClient/GetCurrentVersionFromURL.py
@@ -1,5 +1,17 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import urllib.request
+    
+
+page = "https://raw.githubusercontent.com/xxxMEMESCOEPxxx/NinjasKnightsNecromancers/main/VersionControl/CurrentVersion"
+
+file = urllib.request.urlopen(page)
+deeta = "lol"
+
+for line in file:
+	decoded_line = line.decode("utf-8")
+	deeta = decoded_line
+	print(decoded_line)
 
 hostName = "localhost"
 serverPort = 8080
@@ -12,7 +24,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>" + deeta + "</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
