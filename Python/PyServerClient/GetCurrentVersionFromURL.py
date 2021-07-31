b0f9df7b4253f7c639c9e7f31cbfd21339067a7e
xxxMEMESCOEPxxx <67339620+xxxMEMESCOEPxxx@users.noreply.github.com>
2021-07-31 19:26:23 -0400

    Update

--- Python/PyServerClient/GetCurrentVersionFromURL.py
+++ Python/PyServerClient/GetCurrentVersionFromURL.py
@@ -1,40 +1,56 @@
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import urllib.request
+
+retry = 5
+
+assignVar = 1
+
+
+def end():
+	print("exitting...")
+	exit()
+
+
+
+def GetDeeta():
+	global assignVar
+	global retry
+	if assignVar == 1:
+		retry = 5
+		assignVar = 0
+
+	try:	
+		print("Getting Version Data...")
+
+
+
     
 
-page = "https://raw.githubusercontent.com/xxxMEMESCOEPxxx/NinjasKnightsNecromancers/main/VersionControl/CurrentVersion"
+		page = "https://raw.githubusercontent.com/xxxMEMESCOEPxxx/NinjasKnightsNecromancers/main/VersionControl/CurrentVersion"
 
-file = urllib.request.urlopen(page)
-deeta = "lol"
+		file = urllib.request.urlopen(page)
+		deeta = "lol"
 
-for line in file:
-	decoded_line = line.decode("utf-8")
-	deeta = decoded_line
-	print(decoded_line)
+		for line in file:
+			decoded_line = line.decode("utf-8")
+			deeta = decoded_line
+			print(decoded_line)
 
-hostName = "localhost"
-serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>" + deeta + "</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+		print("Done")
+	
+	except:
+		print ("Failed to get version information! retrying...")
+		if retry >= 1:
+			retry -= 1
+			GetDeeta()
+		else:
+			print("Get data failed.")
+			end()
+		
+		
+		
+GetDeeta()
