8c15af719be01f9d6daabc6aa22f93252a05ec6e
xxxMEMESCOEPxxx <67339620+xxxMEMESCOEPxxx@users.noreply.github.com>
2021-07-31 20:48:51 -0400

    Update

--- Python/PyServerClient/GetCurrentVersionFromURL.py
+++ Python/PyServerClient/GetCurrentVersionFromURL.py
@@ -1,12 +1,14 @@
-
+import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import urllib.request
 
 retry = 5
 
-assignVar = 1
+currver = sys.argv[1]
 
+assignVar = 1
+print("Getting Version Data...")
 
 def end():
 	print("exitting...")
@@ -17,12 +19,13 @@ def end():
 def GetDeeta():
 	global assignVar
 	global retry
+	global currver
 	if assignVar == 1:
 		retry = 5
 		assignVar = 0
 
 	try:	
-		print("Getting Version Data...")
+
 
 
 
@@ -36,12 +39,17 @@ def GetDeeta():
 		for line in file:
 			decoded_line = line.decode("utf-8")
 			deeta = decoded_line
-			print(decoded_line)
-
+			print("Current newest Version: " + decoded_line)
+			print("Current Game Version: " + currver)
+			
+			if(deeta != (currver + "\n")):
+				print ("New Version Avaliable!")
 
 
 		print("Done")
-	
+		retry = 0
+		return deeta
+		print (deeta)
 	except:
 		print ("Failed to get version information! retrying...")
 		if retry >= 1:
