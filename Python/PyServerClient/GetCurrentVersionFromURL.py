b74e44670c223e4f8defe5142a9507e7934002c6
xxxMEMESCOEPxxx <67339620+xxxMEMESCOEPxxx@users.noreply.github.com>
2021-08-05 17:57:41 -0400

    Update get version

--- Python/PyServerClient/GetCurrentVersionFromURL.py
+++ Python/PyServerClient/GetCurrentVersionFromURL.py
@@ -1,22 +1,35 @@
 import sys
-from http.server import BaseHTTPRequestHandler, HTTPServer
+#from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import urllib.request
 
-retry = 5
+retry = 4
 
-currver = sys.argv[1]
+try:
+	currver = sys.argv[1]
+except:
+	print("CURRENT VERSION INFORMATION NOT AVALIABLE. Please enter version: ")
+	currver = input(">> ")
+	#time.sleep(1)
+	
 
 assignVar = 1
-print("Getting Version Data...")
+
 
 def end():
 	print("exitting...")
 	exit()
 
 
+sayget = 0
+
 
 def GetDeeta():
+	global sayget
+	if sayget == 0:
+		
+		print("Getting Version Data...")
+		sayget = 1
 	global assignVar
 	global retry
 	global currver
@@ -26,43 +39,54 @@ def GetDeeta():
 
 	try:	
 
-
-
-
-    
-
 		page = "https://raw.githubusercontent.com/xxxMEMESCOEPxxx/NinjasKnightsNecromancers/main/VersionControl/CurrentVersion"
 
 		file = urllib.request.urlopen(page)
-		deeta = "lol"
+		deeta = ""
 
 		for line in file:
 			decoded_line = line.decode("utf-8")
 			deeta = decoded_line
-			print("Current newest Version: " + decoded_line)
+			print("Newest Version: " + decoded_line)
 			print("Current Game Version: " + currver)
 			
 			if(deeta != (currver + "\n")):
 				print ("New Version Avaliable!")
 
-
-		print("Done")
+		try:
+			print("Writing Version Data to File...")
+			filetowrite = open("CurrVer.txt", "w")
+			filetowrite.write(deeta)
+			filetowrite.close()
+			print("Done Writing.")
+		except:
+			print("Failed to write data to file!")
+			
+			
+		#print("Finished.")
 		retry = 0
-		fh = open('currver.txt', 'w+')
-		fh.write(deeta)
-		fh.close()
-                
-
-		#print (deeta)
+		try:
+			
+			sys.exit(0)
+			
+		except:
+			print("Non fatal error: exit() failed.")
+			return deeta
+		return deeta
+		print (deeta)
+		
 	except:
-		print ("Failed to get version information! retrying...")
-		if retry >= 1:
+		print ("Failed to get version information! retrying... (", retry, ")")
+		if retry > 1:
 			retry -= 1
+			if retry == 1:
+				1+1
+				#print("FINAL ATTEMPT!")
+			time.sleep(1)
 			GetDeeta()
 		else:
-			print("Get data failed.")
+			print("Getting data failed.")
 			end()
-		
-		
-		
+			
+print("PROGRAM STARTING...")
 GetDeeta()
